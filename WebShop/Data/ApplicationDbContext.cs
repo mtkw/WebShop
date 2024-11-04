427aff696ff701b79b4dca76f0de3e19ca09122e
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-11-04 21:07:15 +0100

    Dodaj pliki projektów.

---
+++ WebShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Models;
+
+
+namespace WebShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ProductCategory> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UEUF85V;Database=WebShop;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ProductCategory>().HasData(
+                new ProductCategory { Id = 1, Name = "Tablets" },
+                new ProductCategory { Id = 2, Name = "Smartphone" },
+                new ProductCategory { Id = 3, Name = "Laptops" },
+                new ProductCategory { Id = 4, Name = "Others" }
+    );
+
+            modelBuilder.Entity<Supplier>().HasData(
+                new Supplier { Id = 1, Name = "Amazon" },
+                new Supplier { Id = 2, Name = "Lenovo" },
+                new Supplier { Id = 3, Name = "Asus" },
+                new Supplier { Id = 4, Name = "Apple" },
+                new Supplier { Id = 5, Name = "Samsung" },
+                new Supplier { Id = 6, Name = "Xiaomi" }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Amazon Fire", Price = 49.9, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ImgPath = "Amazon Fire", ProductCategoryId = 1, SupplierId = 1 },
+                new Product { Id = 2, Name = "Lenovo IdeaPad Miix 700", Price = 479.9, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ImgPath = "Lenovo IdeaPad Miix 700", ProductCategoryId = 3, SupplierId = 2 },
+                new Product { Id = 5, Name = "Lenovo M10", Price = 179.9, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ImgPath = "Lenovo M10", ProductCategoryId = 1, SupplierId = 2 },
+                new Product { Id = 3, Name = "Amazon Fire HD 8", Price = 89.9, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ImgPath = "Amazon Fire HD 8", ProductCategoryId = 1, SupplierId = 1 },
+                new Product { Id = 4, Name = "iPhone 12", Price = 799.9, Currency = "USD", Description = "The latest iPhone with improved performance, better camera, and 5G capabilities.", ImgPath = "iPhone 12", ProductCategoryId = 2, SupplierId = 4 }
+                );
+        }
+    }
+}
---
+++ WebShop/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Currency {  get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+        public int ProductCategoryId { get; set; }
+        [ForeignKey("ProductCategoryId")]
+        [ValidateNever]
+        public ProductCategory Category { get; set; }
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")]
+        [ValidateNever]
+        public Supplier Supplier { get; set; }
+    }
+}
