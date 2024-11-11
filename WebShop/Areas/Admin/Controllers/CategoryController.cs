72c0a71f0bf3fa65fe55e9259ebaf738fc0b9783
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-11-11 09:49:04 +0100

    Lista Kategori - działa Przejscie na podstrone do edycji - działa Usuwanie - NIE DZIAŁA

---
+++ WebShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Models;
+using WebShop.Repository.IRepository;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    [Controller]
+    [Area("Admin")]
+    [Authorize(Roles ="admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork) 
+        { 
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<ProductCategory> categories = _unitOfWork.ProductCategory.GetAll().ToList();
+
+            return View(categories);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id) 
+        {
+            if (id == 0) 
+            { 
+                return NotFound();
+            }
+
+            var categoryToDelete = _unitOfWork.ProductCategory.GetAll().Where(x=>x.Id == id).First();
+            _unitOfWork.ProductCategory.Remove(categoryToDelete);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            var categoryToUpdate = _unitOfWork.ProductCategory.GetAll().Where(x=> x.Id == id).First();
+            return View(categoryToUpdate);
+        }
+    }
+}
