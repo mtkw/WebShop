a16392e3abfdc9c095950d93211acf6979ae016a
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-11-16 18:03:57 +0100

    User-Panel -add user panel navigation bar

---
+++ WebShop/Migrations/20241116163424_ModificationProductTable.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModificationProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Quantity", "UserId" },
+                values: new object[] { 0, null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Quantity", "UserId" },
+                values: new object[] { 0, null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Quantity", "UserId" },
+                values: new object[] { 0, null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Quantity", "UserId" },
+                values: new object[] { 0, null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Quantity", "UserId" },
+                values: new object[] { 0, null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Products");
+        }
+    }
+}
