e41d55614d44e57eaf041adffb321b4f90443987
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-11-16 19:49:18 +0100

    Add rich editor

---
+++ WebShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebShop.Models.Views;
+using WebShop.Models;
+using WebShop.Repository.IRepository;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    [Controller]
+    [Area("Admin")]
+    [Authorize(Roles = "admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.ProductCategory.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product(),
+                SupplierList = _unitOfWork.Supplier.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            if (id == null || id == 0)
+            {
+                //create
+                return View(productVM);
+            }
+            else
+            {
+                //update
+/*                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);*/
+                return View(productVM);
+            }
+        }
+    }
+}
