7e38dbc1328576adbc95b443c24c25a43a66711e
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-11-24 18:27:11 +0100

    Add Cart Icon

---
+++ WebShop/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
