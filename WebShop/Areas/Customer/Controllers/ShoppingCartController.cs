18ed0eaeaffd6bd58579f380ddb6fa7bccb9e40d
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-11-25 22:36:37 +0100

    Shopping Cart page - IN Progress

--- WebShop/Areas/Customer/Controllers/ShoppingCartController.cs
+++ WebShop/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebShop.Models;
+using WebShop.Models.Views;
+using WebShop.Repository.IRepository;
 
 namespace WebShop.Areas.Customer.Controllers
 {
@@ -7,9 +11,50 @@ namespace WebShop.Areas.Customer.Controllers
     [Authorize]
     public class ShoppingCartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            List<ShoppingCart> cartFromDB = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includProperties: "Product").ToList();
+
+            var shoppingCartVM = new ShoppingCartVM()
+            {
+                Products = cartFromDB,
+                TotalCountOfProducts = CalculateCountOfProducts(cartFromDB),
+                TotalPrice = CalculateCartTotalPrice(cartFromDB)
+            };
+
+
+            return View(shoppingCartVM);
         }
+
+        private int CalculateCountOfProducts(List<ShoppingCart> cart) 
+        { 
+            var count = 0;
+            foreach (var cartItem in cart) 
+            {
+                count += cartItem.Count;
+            }
+            return count;
+        }
+
+        private double CalculateCartTotalPrice(List<ShoppingCart> cart) 
+        {
+            double totalPrice = 0;
+            foreach (var cartItem in cart) 
+            {
+                totalPrice += (cartItem.Count * cartItem.Product.Price);
+            }
+            return totalPrice;
+        }
+
     }
 }
