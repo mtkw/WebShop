60165d82d92b791caf763013e30b978613fc5b70
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2025-01-02 20:12:52 +0100

    login error if wrong login or password

--- WebShop/Models/Views/LoginViewModel.cs
+++ WebShop/Models/Views/LoginViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace WebShop.Models.Views
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShop.Models.Views
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
+        public string ErrorMsg { get; set; }
     }
 }
