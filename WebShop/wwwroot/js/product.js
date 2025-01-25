0bfee4bb655fcfaf04eedd53ecde32cb061c7fae
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2025-01-25 21:48:59 +0100

    Admin Panel Order List

--- WebShop/wwwroot/js/product.js
+++ WebShop/wwwroot/js/product.js
@@ -18,7 +18,7 @@ function loadDataTable() {
             {
                 data: 'imgPath',
                 "render": function (data) {
-                    return `<img class="card-img-top" src="/img/${data}" alt="${data}" style="height: 100px;"/>`
+                    return `<img class="card-img-top" src="/img/${data}.jpg" alt="${data}" style="height: 100px;"/>`
                 },
                 "width": "10%"
             },
