3985f4a7fab0bd81509e46d395ba53f9e837308a
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2025-01-31 21:34:24 +0100

    Add Message Controle to prepare data JSON prepare Javascript function to retrive data from Controler and fil modal on _Leyout and _LoginPartial page

---
+++ WebShop/Areas/Customer/Controllers/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.DataAccess.Repository.IRepository;
+
+namespace WebShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class MessageController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MessageController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]   
+        public IActionResult GetAllMessageForUser(string userId)
+        {
+            var messages = _unitOfWork.UsersMessage.GetAll(u => u.UserId == userId).ToList();
+
+            return Json(messages);
+        }
+    }
+}
